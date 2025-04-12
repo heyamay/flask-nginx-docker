abcb889015d9d653569e78f1736a69b24d1e6df7
root <root@AmayJaiswal>
2025-04-12 14:51:45 +0530

    🛠️ Update welcome message for GitHub Actions test

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1>Welcome to Amay's Flask App via Nginx + Docker + AWS!</h1>"
+def hello_world():
+    return "Welcome to Amay's Flask App via Nginx + Docker + AWS! + Github Actions"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
